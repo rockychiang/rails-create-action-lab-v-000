64ba9436b8b0c102671c2e3d471bc1b61e5f038d
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-15 21:10:51 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
 
   def create
     raise params.inspect
-    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
     @student.save
     redirect_to student_path(@student)
   end
